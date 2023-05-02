6284d3b0c355a22011fb8aaf3ea022888e9bf6d0
Ethan Eill <97324170+Ethan-Eill@users.noreply.github.com>
2023-05-01 20:56:31 -0400

    Getting started

--- controller.py
+++ controller.py
@@ -1,15 +1,38 @@
 import pygame
-from view import Main_View
+from views import Main_View
 
 def main():
+    pygame.init()
+    global loop_state, view
     print("Algorithm Visualizer")
     loop_state = "main-menu"
+    view = Main_View()
     while True:
         if loop_state == "main-menu":
             main_menu()
+        elif loop_state == "sorting":
+            print("were sorting")
+        elif loop_state == "options":
+            print("were in options")
+
+        
 
 def main_menu():
-    
+    global loop_state, view
+    mouse_pos = pygame.mouse.get_pos()
+
+    view.main_menu(mouse_pos)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if view.sort_button.checkForInput(mouse_pos):
+                loop_state = "sorting"
+            elif view.options_button.checkForInput(mouse_pos):
+                loop_state = "options"
+            elif view.quit_button.checkForInput(mouse_pos):
+                pygame.quit()
 
 if __name__ == "__main__":
     main()
--- views.py
+++ views.py
@@ -7,17 +7,34 @@ class Main_View():
         self.screen_width, self.screen_height = 1200, 800
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.background_color = (48, 48, 48)
-        self.title_font = pygame.font.Font("assets/ARCADE_R.TTF", 70)
+        self.title_font = pygame.font.Font("assets/ARCADE_N.TTF", 50)
         self.title_color = (247, 17, 232)
         self.button_color = (22, 182, 250)
-        self.button_font = pygame.font.Font("assets/ARCADE_R.TTF", 50)
+        self.button_font = pygame.font.Font("assets/ARCADE_N.TTF", 40)
+        self.information_font = pygame.font.Font("assets/ARCADE_N.TTF", 14)
+        self.information_color = (255,255,255)
+        self.sort_button = Button(image=None, pos=(self.screen_width/2, self.screen_height/1.7), 
+                        text_input="Sort", font=self.button_font, base_color=self.button_color, hovering_color="White")
+        self.options_button = Button(image=None, pos=(self.screen_width/2, self.screen_height/1.5), 
+                        text_input="Options", font=self.button_font, base_color=self.button_color, hovering_color="White")
+        self.quit_button = Button(image=None, pos=(self.screen_width/2, self.screen_height/1.34), 
+                        text_input="Exit", font=self.button_font, base_color=self.button_color, hovering_color="White")
         self.is_sound_on = True
         
         pygame.display.set_caption("Algorithm Visualizer")
 
-    def main_menu(self):
+    #main menu function to be looped through
+    def main_menu(self, mouse_pos):
         self.screen.fill(self.background_color)
-        mouse_pos = pygame.mouse.get_pos()
 
+        title_text = self.title_font.render("Algorithm Visualizer", True, self.title_color)
+        title_text_rect = title_text.get_rect(center=(self.screen_width/2, self.screen_height/8.5))
 
+        self.screen.blit(title_text, title_text_rect)
+
+        for button in [self.sort_button, self.options_button, self.quit_button]:
+            button.changeColor(mouse_pos)
+            button.update(self.screen)
+
+        pygame.display.update()
 
